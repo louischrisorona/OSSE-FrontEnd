a26250134dade38df75740d78f1fc5f415826ceb
Jonathan Martin <oldschoolspaceempire@gmail.com>
2021-07-05 11:17:35 -0500

    Login with Redux and Registration with Redux are not needed

--- src/Redux/Reducers/rootReducer.js
+++ src/Redux/Reducers/rootReducer.js
@@ -1,10 +1,6 @@
 import { combineReducers } from 'redux';
-import registrationReducer from './registrationReducer';
-import loginReducer from './loginReducer';
 
 const rootReducer = combineReducers({
-  registrationReducer,
-  loginReducer,
 });
 
 export default rootReducer;
